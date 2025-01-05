3fdaa8bb1804f5af431a04f97b9491454a1784bf
Elite007 <alijonoveldorbek78@gmail.com>
2025-01-05 17:38:06 +0900

    initial

---
+++ myreact/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import HomeComponent from './components/home'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <HomeComponent />
+  </StrictMode>,
+)
