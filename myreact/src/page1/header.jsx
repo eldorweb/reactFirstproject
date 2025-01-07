29030a6e111e8dbc1cc75f7ee4e7c9290c2d5cf4
Elite007 <alijonoveldorbek78@gmail.com>
2025-01-07 20:02:23 +0900

    ok

--- myreact/src/page1/header.jsx
+++ myreact/src/page1/header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+
 const Header = () => {
   return (
-    <div>header</div>
+    <div className='container header'>
+      <h1>Best food for your taste</h1>
+      <p>Discover delectable cuisine and unforgettable moments in our welcoming, culinary haven.</p>
+      <div className='header_btndiv'>
+        <button>Book A Table</button>
+        <button>Explore Menu</button>
+      </div>
+    </div>
   )
 }
 
--- myreact/src/page1/nav_bottom.jsx
+++ myreact/src/page1/nav_bottom.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import logo from '../assets/svg/logo.svg'
 
 const NavBottom = () => {
     return (
-    <div>navBottom</div>
+    <div className='container nav_bottom'>
+    <div className='logodiv'>
+        <img src={logo}/>
+        <p>Bistro Bliss</p>
+    </div>
+    <div className='nav_bottom_list'>
+        <p>Home</p>
+        <p>About</p>
+        <p>Menu</p>
+        <p>Pages</p>
+        <p>Contact</p>
+    </div>
+    <button className='nav_bottom_btn'>Book A Table</button>
+    </div>
     )
 }
 
